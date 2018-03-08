f09821aad1a1237299839f4b326524fbf79a91f1
Edyta <dudek002@hotmail.com>
2018-03-08 11:34:04 -0600

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegetable burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Pizza burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Fish burger", false);
